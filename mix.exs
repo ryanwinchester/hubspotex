fcb6adba74ce4e09e9ba6234a3dc97782b863ef0
ryanwinchester <fungku@gmail.com>
2016-07-26 14:11:35 -0700

    Add docs

--- mix.exs
+++ mix.exs
@@ -7,6 +7,8 @@ defmodule Hubspot.Mixfile do
      elixir: "~> 1.2",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
+     description: description(),
+     package: package(),
      deps: deps]
   end
 
@@ -28,6 +30,20 @@ defmodule Hubspot.Mixfile do
   # Type "mix help deps" for more examples and options
   defp deps do
     [{:httpoison, "~> 0.9.0"},
-     {:poison, "~> 2.0"}]
+     {:poison, "~> 2.0"},
+     {:ex_doc, ">= 0.0.0", only: :dev}]
+  end
+
+  defp description do
+    """
+    Hubspot API wrapper
+    """
+  end
+
+  defp package do
+    [# These are the default files included in the package
+     maintainers: ["Ryan Winchester"],
+     licenses: ["Apache 2.0"],
+     links: %{"GitHub" => "https://github.com/ryanwinchester/hubspotex"}]
   end
 end

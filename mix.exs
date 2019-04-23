c46467329429f187f05653e2c9f754aa09ad8476
Ole J. Rosendahl <ole@blacktangent.com>
2019-04-23 14:49:51 +0800

    Update depedencies

--- mix.exs
+++ mix.exs
@@ -29,7 +29,7 @@ defmodule Hubspot.Mixfile do
   #
   # Type "mix help deps" for more examples and options
   defp deps do
-    [{:httpoison, "~> 0.9.0"},
+    [{:httpoison, "~> 1.0"},
      {:poison, "~> 2.0"},
      {:ex_doc, ">= 0.0.0", only: :dev}]
   end
